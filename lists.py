c0703c4ad3394fd179f86c659412d7a65d2fefaa
abdulmoizahmedtuc <moiz.ahmedssuet@gmail.com>
2023-09-20 02:39:25 +0200

    update list and tuple file

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-# List is mutable
+# List is mutable, which means you can modify their contents after they are created.
 list_x = [1, 2, 3, 4, 4]
 print(list_x)
 # Print value from specific position
@@ -29,6 +29,60 @@ print(list_x)
 removeAllValues = list_x[:] = []
 print(removeAllValues)
 
+# Extend the existing list with new list
 list_y = [7, 8, 9, 10]
 list_x.extend(list_y)
 print(list_x)
+
+
+# Reverse the list, related to slicing
+def reverse_list(input_list):
+    return input_list[::-1]
+
+
+print(reverse_list([1, 2, 3, 4]))
+
+
+# Find duplicates in list
+def find_duplicates(input_list):
+    duplicates = []
+    for item in input_list:
+        if input_list.count(item) > 1 and item not in duplicates:
+            duplicates.append(item)
+    return duplicates
+
+
+# List comprehension
+# Given a list of numbers, create a new list that contains only the even numbers using list comprehension.
+def even_numbers(input_list):
+    return [x for x in input_list if x % 2 == 0]
+
+
+# Remove duplicates from lists
+# Write a function that takes a list as input and returns a new list with duplicates removed.
+def remove_duplicates(input_list):
+    return list(set(input_list))
+
+
+# Rotate list elements
+# Given a list and an integer k, rotate the list to the right by k steps.
+def rotate_list(input_list, k):
+    k = k % len(input_list)
+    return input_list[-k:] + input_list[:-k]
+
+
+# Merge two sorted list
+# Merge two sorted lists into a new sorted list.
+def merge_sorted_lists(list1, list2):
+    merged = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged.append(list1[i])
+            i += 1
+        else:
+            merged.append(list2[j])
+            j += 1
+    merged.extend(list1[i:])
+    merged.extend(list2[j:])
+    return merged
--- tuples.py
+++ tuples.py
@@ -1,7 +1,40 @@
-# Tuple is immutable
-tuple = (1, 2, 3, 4, 5)
-print(tuple)
+# Tuple is immutable, Tuples are similar to lists, but once you create a tuple, you cannot change its elements
+tuples = (1, 2, 3, 4, 5)
+print(tuples)
+
 
 # TypeError: 'tuple' object does not support item assignment
-tuple[4] = 6
-print(tuple)
+# tuples[4] = 6
+# print(tuples)
+
+
+# Swap Two Values with a Tuple:
+# Problem: Write a function that takes two values and swaps them using a tuple.
+def swap_values(a, b):
+    return b, a
+
+
+print(swap_values(1, 2))
+
+
+# Find the Largest and Smallest Values in a Tuple:
+# Problem: Write a function that takes a tuple of numbers and returns the largest and smallest values as a tuple.
+
+def find_extremes(numbers):
+    return min(numbers), max(numbers)
+
+
+random_numbers = (1, 2, 3, 4, 5, 6)
+print(find_extremes(random_numbers))
+
+
+# Tuple Concatenation:
+# Problem: Write a function that concatenates two tuples and returns a new tuple.
+
+def concatenate_tuples(tuple1, tuple2):
+    return tuple1 + tuple2
+
+
+tuple_abc = (1, 2, 3)
+tuple_xyz = (4, 5, 6)
+print(concatenate_tuples(tuple_abc, tuple_xyz))
